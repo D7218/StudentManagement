0aebaee730db01958952351865316a2e63b06178
om Dubbe <dubbeom@gmail.com>
2025-02-13 23:31:37 +0530

    new chenges

---
+++ strudentManagement/strudentManagement/src/main/java/com/springManagementProject/Controller/StudentController.java
@@ -0,0 +1,15 @@
+package com.springManagementProject.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/studentManagement")
+public class StudentController {
+
+    @GetMapping("/student")
+    public String studentInfo(){
+        return "this is college information";
+    }
+}
